2cadcf659bb2a837ac5b565db105f46067541479
Nicholas Cantelmi <ncantelmi@Nicholass-MacBook-Air-2.local>
2014-10-16 15:22:08 -0700

    Added comments to methods and removed a couple unnecessary variables that I no-longer needed.

--- sudoku_algorithm.rb
+++ sudoku_algorithm.rb
@@ -1,14 +1,14 @@
 class Sudoku
-  attr_accessor :board_string, :boxes, :any_solved, :index
+  attr_accessor :board_string, :boxes, :index
   POTENTIALS = ("1".."9").to_a
 
   def initialize(board_string)
     @board_string = board_string
     @boxes = []
-    @any_solved = false
     @index = 0
   end
 
+  # Series of steps to display and solve the board
   def solve!
     before = Time.now
     solve_with_recursive_backtracking
@@ -17,16 +17,19 @@ class Sudoku
     puts "Solved this board in: \n #{Time.now - before}"
   end
 
+  # Checks if the board is complete and returns true if there are no zeros left
   def solved?
     return true unless board_string.include?("0")
   end
 
-  def display
+  # This clears the terminal screen so solving of board is viewable
+  def clear_then_display
     print "\e[2J"
     print "\e[H"
     show_board
   end
 
+  # Constructs a board that for viewing in terminal
   def show_board
     b = board_string.clone
     b.split(//).each_with_index do |num, index|
@@ -39,6 +42,7 @@ class Sudoku
 
   private
 
+  # Finds the next zero with the least number of possible solutions. If it has only 1 possible answer, it instantly uses it.  Return value is the index position of the number in board_string
   def next_zero
     smallest_index = nil
     smallest = 9
@@ -58,8 +62,9 @@ class Sudoku
     smallest_index
   end
 
+  # Uses Recursive Backtracking to solve the board. Returns true if solved and false otherwise.
   def solve_with_recursive_backtracking
-    display
+    clear_then_display
     next_to_guess = next_zero
     if solved?
       return true
@@ -79,6 +84,7 @@ class Sudoku
     false
   end
 
+  # Returns true if check_row, check_col, and check_box all pass
   def check_all(number, index)
     return false if !check_row(number, index)
     return false if !check_col(number, index)
@@ -123,12 +129,15 @@ class Sudoku
     return true
   end
 
+  # Builds up the rows to be checked
   def create_rows
     rows = []
     board_copy = board_string.clone
     9.times do rows << board_copy.slice!(0..8) end
     rows
   end
+
+  # Builds up the columns to be checked
   def create_cols
     cols = []
     (0..9).each do |column|
@@ -136,6 +145,8 @@ class Sudoku
     end
     cols
   end
+
+  # Builds up the boxes to be checked
   def create_boxes
     boxes = []
     (0..2).each do |vert_offset|
@@ -151,8 +162,8 @@ end  # End of Sudoku class
 # board_string = "030500804504200010008009000790806103000005400050000007800000702000704600610300500"
 # board_string = "300000000050703008000028070700000043000000000003904105400300800100040000968000200"
 
-# Peter Norvig 11 hardest boards
 
+# Peter Norvig 11 hardest boards
 norvig_11_hardest_boards = %w(850002400720000009004000000000107002305000900040000000000080070017000000000036040
                               005300000800000020070010500400005300010070006003200080060500009004000030000009700
                               120040000005069010009000500000000070700052090030000002090600050400900801003000904
